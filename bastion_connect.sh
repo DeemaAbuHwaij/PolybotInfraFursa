a97410ff25f1290fc55ab280476873fc1968da7b
deema <demaabohwej@gmail.com>
2025-05-08 18:50:34 +0300

    bastion connect

--- bastion_connect.sh
+++ bastion_connect.sh
@@ -15,15 +15,14 @@ if [ -z "$2" ]; then
   exit 5
 fi
 
-if [ $# -lt 3 ]; then
+if [ -z "$3" ]; then
   echo "Please provide a command to run on the target"
   exit 5
 fi
 
 BASTION_IP="$1"
 TARGET_PRIVATE_IP="$2"
-shift 2
+COMMAND="$3"
 
-ssh -i "$KEY_PATH" -o StrictHostKeyChecking=no \
-  -o ProxyCommand="ssh -i $KEY_PATH -o StrictHostKeyChecking=no -W %h:%p ubuntu@$BASTION_IP" \
-  ubuntu@"$TARGET_PRIVATE_IP" "$@"
+# Execute command on target instance through the bastion
+ssh -i "$KEY_PATH" ubuntu@"$TARGET_PRIVATE_IP" "$COMMAND"
