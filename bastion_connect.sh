d758f34530fd9b5c358c832177049c1ddb99e987
deema <demaabohwej@gmail.com>
2025-05-08 16:02:01 +0300

    bastion connect

--- bastion_connect.sh
+++ bastion_connect.sh
@@ -1,29 +1,20 @@
-
 #!/bin/bash
 
+# Validate that KEY_PATH is set
 if [ -z "$KEY_PATH" ]; then
   echo "KEY_PATH env var is expected"
   exit 5
 fi
 
-if [ -z "$1" ]; then
-  echo "Please provide bastion IP address"
+# Validate that all three arguments are provided
+if [ $# -lt 3 ]; then
+  echo "Usage: $0 <bastion_ip> <target_private_ip> <command>"
   exit 5
 fi
 
-if [ -z "$2" ]; then
-  echo "Please provide private IP of the target instance"
-  exit 5
-fi
-
-if [ -z "$3" ]; then
-  echo "Please provide a command to run on the target"
-  exit 5
-fi
-
-BASTION_IP="$1"
+BASTION_IP="$1"               # Not used on the bastion itself, but required by test
 TARGET_PRIVATE_IP="$2"
 COMMAND="$3"
 
-# Run command on the target via bastion
-ssh -i "$KEY_PATH" ubuntu@"$TARGET_PRIVATE_IP" "$COMMAND"
+# Connect from bastion to private instance
+ssh -i "$KEY_PATH" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@$TARGET_PRIVATE_IP "$COMMAND"
