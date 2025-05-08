0232e86219da92216f051597bf63c342e5a575ed
deema <demaabohwej@gmail.com>
2025-05-08 15:53:12 +0300

    bastion connect

--- bastion_connect.sh
+++ bastion_connect.sh
@@ -1,19 +1,29 @@
+
 #!/bin/bash
 
-# Check if the SSH key path environment variable is set
 if [ -z "$KEY_PATH" ]; then
-  echo "❌ Error: KEY_PATH environment variable not set."
-  echo "Please run: export KEY_PATH=/path/to/your/key.pem"
+  echo "KEY_PATH env var is expected"
   exit 5
 fi
 
-# Bastion and target instance details (hardcoded)
-BASTION_USER=ubuntu
-BASTION_IP=18.144.5.190            # Bastion public IP
-TARGET_USER=ubuntu
-TARGET_PRIVATE_IP=10.0.0.50      # Polybot private IP
+if [ -z "$1" ]; then
+  echo "Please provide bastion IP address"
+  exit 5
+fi
 
-# SSH into the target instance via the Bastion host
-ssh -i "$KEY_PATH" -o ProxyCommand="ssh -i $KEY_PATH -W %h:%p $BASTION_USER@$BASTION_IP" $TARGET_USER@$TARGET_PRIVATE_IP
+if [ -z "$2" ]; then
+  echo "Please provide private IP of the target instance"
+  exit 5
+fi
 
+if [ -z "$3" ]; then
+  echo "Please provide a command to run on the target"
+  exit 5
+fi
 
+BASTION_IP="$1"
+TARGET_PRIVATE_IP="$2"
+COMMAND="$3"
+
+# Run command on the target via bastion
+ssh -i "$KEY_PATH" ubuntu@"$TARGET_PRIVATE_IP" "$COMMAND"
