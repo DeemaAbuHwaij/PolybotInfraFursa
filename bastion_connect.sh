96cd98691f83ec1a8c8e835e004a82447e33fc9b
deema <demaabohwej@gmail.com>
2025-05-08 14:55:45 +0300

    bastion connect

--- bastion_connect.sh
+++ bastion_connect.sh
@@ -11,7 +11,7 @@ fi
 BASTION_USER=ubuntu
 BASTION_IP=18.144.5.190            # Bastion public IP
 TARGET_USER=ubuntu
-TARGET_PRIVATE_IP=10.0.1.164       # Polybot private IP
+TARGET_PRIVATE_IP=10.0.0.50      # Polybot private IP
 
 # SSH into the target instance via the Bastion host
 ssh -i "$KEY_PATH" -o ProxyCommand="ssh -i $KEY_PATH -W %h:%p $BASTION_USER@$BASTION_IP" $TARGET_USER@$TARGET_PRIVATE_IP
