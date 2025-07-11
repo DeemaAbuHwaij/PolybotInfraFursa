c2cfbbdc0e22d978afaace529a06ac917a88d6c5
deema <demaabohwej@gmail.com>
2025-07-11 09:40:23 +0300

    Add prod deployment

--- tf/modules/k8s-cluster/main.tf
+++ tf/modules/k8s-cluster/main.tf
@@ -365,13 +365,11 @@ resource "aws_launch_template" "worker" {
     }
   }
 
-  network_interfaces {
-    associate_public_ip_address = true
-    security_groups = [
-      aws_security_group.control_plane_sg.id,
-      aws_security_group.worker_sg.id
-    ]
-  }
+  vpc_security_group_ids = [
+  aws_security_group.control_plane_sg.id,
+  aws_security_group.worker_sg.id
+  ]
+
 
   tag_specifications {
     resource_type = "instance"
